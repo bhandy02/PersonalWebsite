8a45e5f78a38ea6c13eabd45bf3c50946f3f822d
Brian Handy <brianhandy02@gmail.com>
2021-05-30 11:47:37 -0400

    Reduce UUID length to 8 characters, to reduce bucket name length

--- packages/cdk/lib/static-website.ts
+++ packages/cdk/lib/static-website.ts
@@ -102,12 +102,8 @@ export class StaticWebsite extends Construct {
 
 function uuid() {  
     var uuidValue = "", k, randomValue;  
-    for (k = 0; k < 32;k++) {  
-      randomValue = Math.random() * 16 | 0;  
-    
-      if (k == 8 || k == 12 || k == 16 || k == 20) {  
-        uuidValue += "-"  
-      }  
+    for (k = 0; k < 8;k++) {  
+      randomValue = Math.random() * 16 | 0;     
       uuidValue += (k == 12 ? 4 : (k == 16 ? (randomValue & 3 | 8) : randomValue)).toString(16);  
     }  
     return uuidValue;  
